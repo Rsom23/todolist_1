56725e53c75d67fda760c3db9f0ae66fe24bb936
Rsom23 <someday150907@gmail.com>
2021-05-03 21:12:04 +0900

    JS START

---
+++ todolist_1.js
@@ -0,0 +1,11 @@
+const todoForm = document.querySelector(".todoForm");
+const todoInput = todoForm.querySelector("input");
+const todoList = document.querySelector(".todoList");
+
+
+function init() {
+
+    
+}
+
+init();

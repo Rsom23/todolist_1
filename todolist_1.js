bd0a69a14c0957ab6c4d2ea6b2d3c6e030488ea9
Rsom23 <rsom2345@gmail.com>
2021-05-04 15:06:03 +0900

    [update] function init

--- todolist_1.js
+++ todolist_1.js
@@ -1,11 +1,35 @@
-const todoForm = document.querySelector(".todoForm");
-const todoInput = todoForm.querySelector("input");
-const todoList = document.querySelector(".todoList");
+const todoForm = document.querySelector(".todoForm");   // form 태그
+const todoInput = todoForm.querySelector("input");      // form > input 태그
+const todoList = document.querySelector(".todoList");   // ul 태그
+
+
 
 
 function init() {
 
-    
+    const li = document.createElement("li");
+    const inputValue = todoInput.value;
+    const textNode = document.createTextNode(inputValue);
+    li.appendChild(textNode);
+
+    if (inputValue === '') {
+        alert ('Write to-do list');
+    } else {
+        todoList.appendChild(li);
+    }
+    inputValue = "";
+
+    const span = document.createElement("span");
+    const text = document.createTextNode("❌");
+    span.className = "close";
+    span.appendChild(text);
+    li.appendChild(span);
+
+    for (let i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            const div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
 }
 
-init();
